bf11efdfe9573af161d3a95c65f5b8f25cc2b3b9
Roy A Vanegas <roy@thecodeeducators.com>
2018-10-20 14:22:47 -0400

    Add a shape that is similar to a line

--- js/sketch.js
+++ js/sketch.js
@@ -8,4 +8,17 @@ function setup() {
 function draw() {
     /* Set the color of the stroke. */
     stroke(198,157,101);
+
+    /* Don’t fill in the color of the shape */
+    noFill();
+
+    /* Begin the shape created by the three vertices. */
+    beginShape();
+    vertex(291, 1);
+    vertex(322, 64);
+    vertex(330, 82);
+    vertex(352, 122);
+
+    /* End the shape created by the three vertices, without a fill-in color (see line 13). */
+    endShape();
 }
